81a001445ad96529389c012a3e715faf77e513cb
AofAab <thanayoot.boon@bumail.net>
2023-11-16 21:56:27 +0700

    PAITUMSA

---
+++ Assets/BU/TOMATO/MidiGameplayExample/Scripts/ComboPanel.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Notero.RaindropGameplay.UI
+{
+    public class ComboPanel : MonoBehaviour
+    {
+        public Image healthbar;
+        public Image[] healthPoints;
+        public Image BG; // Reference ของพื้นหลังเดิม
+        public Image FadeBG; // Reference ของพื้นหลังที่ต้องการเปลี่ยน
+
+        float health = 0;
+        float maxHealth = 110000;
+        float lerpSpeed;
+
+        void Start()
+        {
+            health = 0;
+        }
+
+        void HealthBarFiller()
+        {
+            healthbar.fillAmount = Mathf.Lerp(healthbar.fillAmount, health / maxHealth, lerpSpeed);
+
+            for (int i = 0; i < healthPoints.Length; i++)
+            {
+                healthPoints[i].enabled = !DisplayHealthPoint(health, i);
+            }
+        }
+
+        bool DisplayHealthPoint(float _health, int pointNumber)
+        {
+            return ((pointNumber * 3900) >= _health);
+        }
+
+        public void Damage(float damagePoints)
+        {
+            if (health > 0)
+                health -= damagePoints;
+        }
+
+        public void Heal(float healingPoints)
+        {
+            health = healingPoints;
+            if (health > maxHealth) health = maxHealth;
+
+            lerpSpeed = 1f * Time.deltaTime;
+
+            HealthBarFiller();
+
+            if (health >= 40000)
+            {
+                BG.gameObject.SetActive(false); // ปิดการใช้งาน BG
+                FadeBG.gameObject.SetActive(true); // เปิดการใช้งาน FadeBG
+            }
+        }
+    }
+}
